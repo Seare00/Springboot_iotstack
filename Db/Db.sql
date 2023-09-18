2a4a2f6ec78b0d5ca881e65cadf6000212c8e73b
Kkabylion <113334216+Kkabylion@users.noreply.github.com>
2023-09-18 13:27:47 +0200

    Update Db.sql

--- Db/Db.sql
+++ Db/Db.sql
@@ -1,1 +1,11 @@
+--Tabell
+drop database if exists tempsensor;
+create database tempsensor;
+use tempsensor;
 
+create table tempsensor (
+    id int auto_increment primary key,
+    sensor_id varchar(100) not null,
+    tid timestamp default current_timestamp,
+    temperatur decimal(5,2) not null
+);
