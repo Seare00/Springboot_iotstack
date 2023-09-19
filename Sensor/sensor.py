85a9fe12e9ea0d0c2cc6b6c3a268e9fde10f9132
Kkabylion <113334216+Kkabylion@users.noreply.github.com>
2023-09-19 18:24:56 +0200

    sensor.py

---
+++ Sensor/sensor.py
@@ -0,0 +1,36 @@
+import serial
+import mysql.connector
+
+# Set up database connection
+db = mysql.connector.connect(
+    host="localhost",
+    user="arduino",
+    password="password",
+    database="sensors"
+)
+
+cursor = db.cursor()
+
+#  COM ändras beroende på vilken din arduino IDE använder.
+ser = serial.Serial('COM4', 9600)
+
+try:
+    while True:
+        # Läser en linje av arduino temperatur datan)
+        line = ser.readline().decode('utf-8').strip()
+        
+        try:
+            temperatur = float(line)
+            query = "INSERT INTO temperatur (Temp) VALUES (%s)"
+            cursor.execute(query, (temperatur,))
+            db.commit()
+            print(f"Sparar temp {temperatur} i SQL DB.")
+        except ValueError:
+            #Felaktig dataformat, triggar denna meddelande.
+            print(f"Felaktig data: {line}")
+
+except KeyboardInterrupt:
+    print("\nStänger...")
+    ser.close()
+    cursor.close()
+    db.close()
