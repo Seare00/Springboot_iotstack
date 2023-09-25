7b62f93812a787af259fc54c926a82db5d08d19b
Kkabylion <113334216+Kkabylion@users.noreply.github.com>
2023-09-25 04:14:22 +0200

    sketch_sep24a_DHT11.ino

--- Sensor/sketch_sep24a_DHT11.ino
+++ Sensor/sketch_sep24a_DHT11.ino
@@ -1,9 +1,9 @@
-#include <DHT.h>  // Include the DHT library
+#include <DHT.h>  
 
-#define DHTPIN A5       // Define which pin the sensor is connected to
-#define DHTTYPE DHT11  // Define the type of DHT sensor (change to DHT11 if you have a DHT11)
+#define DHTPIN A5       
+#define DHTTYPE DHT11  
 
-DHT dht(DHTPIN, DHTTYPE);  // Initialize the DHT sensor
+DHT dht(DHTPIN, DHTTYPE);  
 
 void setup() {
   Serial.begin(9600);  // Start the Serial communication at 9600 baud rate
@@ -16,7 +16,7 @@ void loop() {
 
 
 
-  // Check if any reads failed and exit early (to try again).
+ 
   if (isnan(t) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
